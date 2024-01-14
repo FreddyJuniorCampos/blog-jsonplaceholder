41cb5d60a9df6b78231ed7210d856456bd4e6e90
Freddy Campos <freddycampos199@gmail.com>
2024-01-14 17:24:10 +0100

    refactor(NAVIGATION): :recycle: adjust navbar

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,7 +1,8 @@
 "use client";
 import { useState } from "react";
 
-import Link from "next/link";
+import { usePathname } from "next/navigation";
+import NextLink from "next/link";
 import Image from "next/image";
 
 import AppBar from "@mui/material/AppBar";
@@ -10,11 +11,9 @@ import Toolbar from "@mui/material/Toolbar";
 import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
 import Menu, { MenuProps } from "@mui/material/Menu";
-import Container from "@mui/material/Container";
-import Button from "@mui/material/Button";
 import MenuItem from "@mui/material/MenuItem";
 import { TbMenu2 } from "react-icons/tb";
-import { alpha, styled } from "@mui/material";
+import { alpha, Link, styled } from "@mui/material";
 
 const pages = [
   {
@@ -66,6 +65,7 @@ const StyledMenu = styled((props: MenuProps) => (
 
 export const Navbar = () => {
   const [anchorElNav, setAnchorElNav] = useState<null | HTMLElement>(null);
+  const path = usePathname();
 
   const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
     setAnchorElNav(event.currentTarget);
@@ -76,85 +76,87 @@ export const Navbar = () => {
   };
 
   const Logo = (
-    <Link className="cursor:pointer" href="/">
-      <Image alt="logo" src="/assets/blog-logo.png" width={60} height={60} />
-    </Link>
+    <NextLink className="cursor:pointer" href="/">
+      <Image alt="logo" src="/assets/blog-logo.png" width={50} height={50} />
+    </NextLink>
   );
 
   return (
     <AppBar position="static">
-      <Container maxWidth="xl">
-        <Toolbar disableGutters>
-          <Box>{Logo}</Box>
+      <Toolbar disableGutters sx={{ maxWidth: 1500, paddingX: 4, gap: 2 }}>
+        <Box sx={{ py: 1 }}>{Logo}</Box>
 
-          <Box
+        <Box
+          sx={{
+            flexGrow: 1,
+            display: { xs: "flex", md: "none" },
+            justifyContent: "flex-end",
+          }}
+        >
+          <IconButton
+            size="large"
+            aria-controls="menu-appbar"
+            aria-haspopup="true"
+            onClick={handleOpenNavMenu}
+            color="inherit"
+          >
+            <TbMenu2 />
+          </IconButton>
+
+          <StyledMenu
+            id="menu-appbar"
+            anchorEl={anchorElNav}
+            anchorOrigin={{
+              vertical: "bottom",
+              horizontal: "left",
+            }}
+            keepMounted
+            transformOrigin={{
+              vertical: "top",
+
+              horizontal: "left",
+            }}
+            open={Boolean(anchorElNav)}
+            onClose={handleCloseNavMenu}
             sx={{
-              flexGrow: 1,
-              display: { xs: "flex", md: "none" },
-              justifyContent: "flex-end",
+              color: "black",
+              display: { xs: "block", md: "none" },
             }}
           >
-            <IconButton
-              size="large"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              onClick={handleOpenNavMenu}
-              color="inherit"
-            >
-              <TbMenu2 />
-            </IconButton>
-
-            <StyledMenu
-              id="menu-appbar"
-              anchorEl={anchorElNav}
-              anchorOrigin={{
-                vertical: "bottom",
-                horizontal: "left",
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: "top",
-
-                horizontal: "left",
-              }}
-              open={Boolean(anchorElNav)}
-              onClose={handleCloseNavMenu}
-              sx={{
-                color: "black",
-                display: { xs: "block", md: "none" },
-              }}
-            >
-              {pages.map((page) => (
-                <Link href={page.href} key={page.label}>
-                  <MenuItem onClick={handleCloseNavMenu}>
-                    <Typography textAlign="center">{page.label}</Typography>
-                  </MenuItem>
-                </Link>
-              ))}
-            </StyledMenu>
-          </Box>
-
-          <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
             {pages.map((page) => (
               <Link href={page.href} key={page.label}>
-                <Button
-                  onClick={handleCloseNavMenu}
-                  sx={{
-                    my: 2,
-                    color: "white",
-                    display: "block",
-                    ":hover": {
-                      bgcolor: "primary.dark",
-                    },
-                  }}
-                >
-                  {page.label}
-                </Button>
+                <MenuItem onClick={handleCloseNavMenu}>
+                  <Typography textAlign="center">{page.label}</Typography>
+                </MenuItem>
               </Link>
             ))}
-          </Box>
-        </Toolbar>
-      </Container>
+          </StyledMenu>
+        </Box>
+
+        <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
+          {pages.map((page) => (
+            <NextLink href={page.href} key={page.label}>
+              <Link
+                component="button"
+                onClick={handleCloseNavMenu}
+                sx={{
+                  px: 2,
+                  height: 66,
+                  bgcolor: page.href === path ? "primary.dark" : "inherit",
+                  textDecoration: "none",
+                  color: "white",
+                  display: "block",
+                  ":hover": {
+                    bgcolor: "primary.dark",
+                  },
+                }}
+              >
+                {page.label}
+              </Link>
+            </NextLink>
+          ))}
+        </Box>
+      </Toolbar>
     </AppBar>
   );
 };
